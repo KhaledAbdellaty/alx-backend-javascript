8b2d7e48afd69a12f54efd9bffc81b39309e50c1
KhaledAbdellaty <kabdellaty@gmail.com>
2024-06-27 17:16:37 +0300

    6. Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,10 +3,8 @@ import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, filename) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
-    .then((res) => {
-      return res.map((val) => ({
-        status: val.status,
-        value: val.status === 'fulfilled' ? val.value : String(val.reason),
-      }));
-    });
+    .then((res) => res.map((val) => ({
+      status: val.status,
+      value: val.status === 'fulfilled' ? val.value : String(val.reason),
+    })));
 }
