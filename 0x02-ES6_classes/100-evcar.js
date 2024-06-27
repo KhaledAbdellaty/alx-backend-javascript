b0c6c7d2708e5f4362d882a98fe941393617d648
KhaledAbdellaty <kabdellaty@gmail.com>
2024-06-27 19:14:16 +0300

    11. EVCar

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -3,12 +3,11 @@ import Car from './10-car';
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
-    this.range = range;
+    this._range = range;
   }
 
   cloneCar() {
     const Species = super.constructor[Symbol.species];
-
     return new Species();
   }
 }
