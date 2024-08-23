90406964b21eb97644b28055c2f44b327312fd45
KhaledAbdellaty <kabdellaty@gmail.com>
2024-08-23 23:07:12 +0300

    8. Organize a complex HTTP server using Express

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -7,8 +7,8 @@ class StudentsController {
     const dataPath = process.argv.length > 2 ? process.argv[2] : '';
     readDatabase(dataPath).then((data) => {
       response.write('This is the list of our students\n');
-      response.write(`Number of students in CS: ${data.CS}. List: ${data.CS}\n`);
-      response.write(`Number of students in SWE: ${data.SWE}. List: ${data.SWE}\n`);
+      response.write(`Number of students in CS: ${data.CS.length}. List: ${data.CS}\n`);
+      response.write(`Number of students in SWE: ${data.SWE.length}. List: ${data.SWE}\n`);
     }).catch((err) => response.status(500)
       .send(err instanceof Error ? err.message : err.toString()))
       .finally(() => {
@@ -23,6 +23,7 @@ class StudentsController {
     if (major !== 'CS' && major !== 'SWE') {
       response.status(500)
         .send('Major parameter must be CS or SWE\n');
+      response.end();
       return;
     }
     readDatabase('./database.csv').then((data) => {
